269d37271e4996f7cf9046e46c0e474917396027
nabiursiddique <nabiursiddique01@gmail.com>
2022-08-22 21:28:55 +0530

    budget section javascript code is completed

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,37 @@
+//function for getting element from input field
+function getInputFieldValue(idName){
+    const inputField = document.getElementById(idName);
+    const inputFieldString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldString);
+    return inputFieldValue;
+}
+
+
 //Player cost calculation
+document.getElementById('player-cost-button').addEventListener('click', function(){
+    //Getting the value of input field from player cost
+    const playerCost = getInputFieldValue('player-input-field');
+    
+    const playerCostTotal = playerCost*5;
+    let totalPlayerCost = document.getElementById('player-cost-total');
+    totalPlayerCost.innerText = playerCostTotal;
+});
+
+//Manager and coach cost and player cost total
+document.getElementById('total-calculation').addEventListener('click',function(){
+    //for getting manager input field value
+    const managerCost = getInputFieldValue('manager-field');
+
+    //For getting coach input field value
+    const coachCost = getInputFieldValue('coach-field');
+
+    //Getting the value of total player cost
+    const totalPlayerCostString = document.getElementById('player-cost-total');
+    const totalPlayerInnerText = totalPlayerCostString.innerText;
+    const playerCost = parseFloat(totalPlayerInnerText);
+
+    //calculating total
+    const totalCost = playerCost + managerCost + coachCost;
+    const totalMoney = document.getElementById('total-money');
+    totalMoney.innerText = totalCost;
+});
